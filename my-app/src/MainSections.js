3ebd201aa6940289aecd69ee02a097a02b6263c6
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-11-06 16:16:03 +0000

    Deleting comments

--- my-app/src/MainSections.js
+++ my-app/src/MainSections.js
@@ -26,14 +26,3 @@ export default function MainSections() {
     </div>
   );
 }
-
-{
-  /* <div className="col-5">
-<Section2 />
-</div>
-<div className="col-1 d-none d-lg-block vertical_line"></div>
-<div className="col d-lg-none horizontal_line"></div>
-<div className="col-5">
-<Section3 />
-</div> */
-}

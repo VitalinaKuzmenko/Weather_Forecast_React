ac66edc60f138911c44daa53b8171e3ddd348b7d
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-03-03 16:56:49 +0000

    Deleted unnecessary line from react-bootstrap

--- src/MainWindow.js
+++ src/MainWindow.js
@@ -3,16 +3,13 @@ import "./MainWindow.css";
 import Section1 from "./Section1";
 import MainSections from "./MainSections";
 import Section4 from "./Section4";
-import Container from "react-bootstrap/Container";
 
 export default function MainWindow() {
   return (
     <div className="MainWindow">
-      <Container>
-        <Section1 />
-        <MainSections />
-        <Section4 />
-      </Container>
+      <Section1 />
+      <MainSections />
+      <Section4 />
     </div>
   );
 }
